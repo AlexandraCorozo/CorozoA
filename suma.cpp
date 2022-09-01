46fff0703748572cb3ec18581f227eccd9b7ca4a
Alexandra Corozo <alexandra.corozo@utelvt.edu.ec>
2022-09-01 14:04:58 -0500

    Programa Suma

--- suma.cpp
+++ suma.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int main()
 {
-	cout<<endl<<endl;
-	cout<<"Hola mundo soy Alexandra";
-	cout<<endl<<endl;
-	return(0);
+	float A,B,C;
+	cin>>A>>B;
+	C=A+B;
+	cout<<C;
+	return 0;
 }
